9a88ed4a141b7db990a7c0caba85119819b4615d
ralph <dejecacionralph@gmail.com>
2026-04-05 22:07:11 +0800

    updated

--- server/index.js
+++ server/index.js
@@ -7,12 +7,14 @@ const app = express();
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
+
 
 const router = require("./routes");
 app.use("/api", router);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 const port = process.env.PORT || 5000;
 
